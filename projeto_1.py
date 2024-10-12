992c17e05c5ef1e0b0bf7922bde9c8ea3a761041
Laura-C-M <shark01963@sharkcoders.pt>
2024-10-12 09:42:37 +0100

    Add files via upload

---
+++ projeto_1.py
@@ -0,0 +1,26 @@
+print("Olá Mundo!")
+
+
+n1 = 5
+n2 = 4
+soma = (n1 + n2)/9
+print("A soma é",soma)
+
+nome ="Alice"
+print("Olá", nome)
+
+
+sum = soma +     1
+print("A soma é",sum)
+
+num1 = 2
+num2 = 2
+print("A soma de" , num1 , "com" , num2 , "é", num1 + num2)
+print("A subtração é" ,num1 - num2)
+print("A multiplicação é" ,num1 * num2)
+print("A divisão é" ,num1 / num2)
+#bomdia
+"""
+olgullbbkl
+lkvlhkkg
+"""
